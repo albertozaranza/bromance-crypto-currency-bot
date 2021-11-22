bf7b26c503d1ed9cc772b2549d671e84e5212b48
Alberto Zaranza <albertozaranza@outlook.com>
2021-11-22 12:19:15 -0300

    fix: 🐛 revert to older version

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,8 @@
-import { Context, Telegraf } from 'telegraf';
+import { Context, Markup, Telegraf } from 'telegraf';
 import { Update } from 'typegram';
 import axios from 'axios';
-import getTokens from './services/getTokens';
+
+import { TOKENS } from './tokens';
 
 const token: string = process.env.BOT_TOKEN as string;
 
@@ -17,21 +18,26 @@ type Token = {
 };
 
 bot.command('price', async ctx => {
-  const tokenName = ctx.message.text.split(' ')[1].toUpperCase();
+  const tokenName = ctx.message.text.split(' ')[1].toLowerCase();
+  const token = TOKENS[tokenName as keyof typeof TOKENS];
 
-  const url = process.env.API_PANCAKESWAP || '';
-
-  try {
-    const token = await getTokens(tokenName);
-
-    const response = await axios.get<Token>(`${url}/tokens/${token}`);
-
-    ctx.replyWithMarkdown(
-      `O preço do *${tokenName}* é: $${parseFloat(
-        response.data.data.price,
-      ).toFixed(2)}`,
-    );
-  } catch (error) {
-    return ctx.reply('Aconteceu um erro ao obter os dados da API');
+  if (!token) {
+    return ctx.reply('O token não existe');
   }
+
+  const response = await axios.get<Token>(process.env.API_PANCAKESWAP + token);
+
+  ctx.replyWithMarkdown(
+    `O preço do *${tokenName}* é: $` +
+      parseFloat(response.data.data.price).toFixed(2),
+  );
+});
+
+bot.command('listtokens', ctx => {
+  ctx.replyWithMarkdown(
+    '*Os tokens disponível são:*\n\n' +
+      Object.keys(TOKENS)
+        .map(token => `- ${token}`)
+        .join('\n'),
+  );
 });
