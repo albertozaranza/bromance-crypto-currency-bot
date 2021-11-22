6d4d6fe2971b1b3d838139c297c902027cb901cd
Alberto Zaranza <albertozaranza@outlook.com>
2021-11-21 22:31:35 -0300

    create commands

---
+++ src/commands.ts
@@ -0,0 +1,35 @@
+import { Context, Markup, Telegraf } from 'telegraf';
+import { Update } from 'typegram';
+import axios from 'axios';
+
+import { TOKENS } from './tokens';
+
+const token: string = process.env.BOT_TOKEN as string;
+
+const bot: Telegraf<Context<Update>> = new Telegraf(token);
+
+type Token = {
+  data: {
+    name: string;
+    symbol: string;
+    price: string;
+    price_BNB: string;
+  };
+};
+
+bot.command('price', async (ctx) => {
+  const token = TOKENS[ctx.message.text as keyof typeof TOKENS];
+
+  if (!token) {
+    return ctx.reply('O token o não existe');
+  }
+  const response = await axios.get<Token>(process.env.API_PANCAKESWAP + token);
+
+  ctx.reply(
+    `O preço do ${token} é: ` + parseFloat(response.data.data.price).toFixed(2)
+  );
+});
+
+bot.command('list-tokens', async (ctx) => {
+  ctx.reply('**Os tokens disponível são:**\n' + Object.keys(TOKENS).join('\n'));
+});
